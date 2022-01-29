32ad20feb83498dedc27d6ff803fc210823a2d66
Dethcrvsh <fabianfabbe@hotmail>
2022-01-29 11:40:03 +0100

    Started implementation of the boxes

---
+++ Box.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+const gravity : int = 7
+
+func _ready():
+	# Make the box not able to rotate
+	set_mode(MODE_CHARACTER)
+	# Make the box able to sleep when there is no movement
+	set_can_sleep(true)
+	# Set the gravity scale
+	set_gravity_scale(gravity)
+	set_continuous_collision_detection_mode(1)
+	
