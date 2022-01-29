be71c32c31d692e77190ff1f4276d21d887b1867
Elias Johansson <eljoh633@student.liu.se>
2022-01-29 22:11:15 +0100

    made main scene

--- wall.gd
+++ wall.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var speed = 100
-
+var speed = 70
+var going = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,4 +10,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	self.position.x -= delta*speed
+	if going:
+		self.position.x -= delta*speed
---
+++ wallDetect.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+func _ready():
+	connect("body_entered", self, "_on_body_entered")
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		get_parent().going = true
+#	pass
