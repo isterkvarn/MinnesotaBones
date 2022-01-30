ed7a591a42ba4ce42e1db0608556e27a174b449c
Elias Johansson <eljoh633@student.liu.se>
2022-01-30 17:01:09 +0100

    added music

--- wallDetect.gd
+++ wallDetect.gd
@@ -6,6 +6,10 @@ func _ready():
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
+		# play escape music
+		body.play_runmusic()
+		var canvas = get_parent().get_parent().get_parent().get_parent().get_node("CanvasModulate")
+		canvas.set_color(Color(0.25, 0.01, 0.01, 1))
 		get_parent().going = true
 		queue_free()
 #	pass
